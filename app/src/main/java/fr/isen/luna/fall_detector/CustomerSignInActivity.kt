fc8e7f610184b76c8ae8752bb04c846944f39388
theodesjardins <theo.desjardins@orange.fr>
2019-03-05 16:39:52 +0100

    creation of customerSignInActivitt

---
+++ app/src/main/java/fr/isen/luna/fall_detector/CustomerSignInActivity.kt
@@ -0,0 +1,12 @@
+package fr.isen.luna.fall_detector
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomerSignInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_sign_in)
+    }
+}
